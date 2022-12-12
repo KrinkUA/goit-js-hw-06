f6622b0cb2eff042b847ae2c679d9f80542f3bbc
Serhii Bratash <krinkua@gmail.com>
2022-12-12 19:59:44 +0200

    task 2 and try task 3

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,9 @@
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +12,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+let ulList = document.getElementById("ingredients");
+const foodIngredient = ingredients.forEach(ingredient => {
+  let items = document.createElement("li");
+  items.innerHTML = ingredient;
+  ulList.append(items);
+});
+
